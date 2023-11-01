dc12ed19255102e3b599ee8164ba6ff29c676e67
Natnael Lema <nlema@miu.edu>
2023-10-31 21:54:16 -0500

    fixing import

--- sd415/d12functions/src/app.ts
+++ sd415/d12functions/src/app.ts
@@ -1,12 +1,24 @@
-// npm run app
-// function add(a: number, b: number): number {
-//     return a + b;
-//     // return console.log(“oh happy day”);
-//    }
-//    const sum1: number = add(10,15);
-//    const sum2: number = add(9, 5 ) ;
-//    console.log("sum1 is: ", sum1, " sum2 is: ", sum2);
 
+import {computeSalesCommission} from "./q1.js"
+console.log("expect 0: ", computeSalesCommission(true, 200));
+console.log("expect 0: ", computeSalesCommission(false, 200));
+console.log("expect 3: ", computeSalesCommission(true, 300));
+console.log("expect 6: ", computeSalesCommission(false, 300));
+console.log("expect 65: ", computeSalesCommission(true, 3500));
+console.log("expect 100: ", computeSalesCommission(false, 3500));
 
-import {computeSalesCommission} from "/github.io/sd415/d12functions/src/computeSalesCommission"
-console.log(computeSalesCommission())
+console.log()
+
+import {compoundInterest} from "./q2.js"
+
+console.log("expect 110.47", compoundInterest(100, 10, 1).toFixed(2));
+console.log("expect 16470.09", compoundInterest(10000, 5, 10).toFixed(2));
+
+console.log()
+
+import {calcDownpayment} from "./q3.js"
+
+console.log("expect 2000: ", calcDownpayment(40000));
+console.log("expect 2500: ", calcDownpayment(50000));
+console.log("expect 7500: ", calcDownpayment(100000));
+console.log("expect 25000: ", calcDownpayment(250000));
