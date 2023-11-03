81688544ebc921c0439b7c4e50579c79201f7f30
Natnael Lema <nlema@miu.edu>
2023-11-02 21:09:25 -0500

    creating function

--- sd415/d14arrays/src/app.ts
+++ sd415/d14arrays/src/app.ts
@@ -1,82 +1,86 @@
+console.log("in app.ts", "sum of [1,2,3] is: ", sum([1, 2, 3]));
 
-
-console.log("in app.ts", "sum of [1,2,3] is: ", sum([1,2,3]));
-/**
- * 
- * @param {number} aa is a number
- * @param {number} b is a number 
- * @param {number} c is a number
- * @returns {number} largest of a, b, c
- */
-export function maxOfThree(a: number, b: number, c: number): number{ 
-   if(a > b && a > c){
+export function maxOfThree(a: number, b: number, c: number): number {
+  if (a >= b && a >= c) {
     return a;
-   }else if(b > a && b > c){
+  } else if (b >= a && b >= c) {
     return b;
-   }else{
+  } else {
     return c;
-   }
+  }
 }
 
-/**
- * 
- * @param {Array} arr of numbers
- * @returns {number} sum of arr numbers
- */
-export function sum(arr: number[]): number{
-    let sum = 0;
-    for(let ele of arr){
-        sum+=ele
+
+export function sum(arr: number[]): number {
+  let sum = 0;
+  for (let ele of arr) {
+    sum += ele;
+  }
+  return sum;
+}
+
+
+export function multiply(arr: number[]): number {
+  let mul = 1;
+  for (let ele of arr) {
+    mul *= ele;
+  }
+  return mul;
+}
+
+
+export function findLongestWord(arr: string[]): number {
+  let longestLength = 0;
+  for (let ele of arr) {
+    const currentLength = ele.length;
+    if (currentLength > longestLength) {
+      longestLength = currentLength;
     }
-    return sum;
+  }
+  return longestLength;
 }
 
 
-/**
- * 
- * @param {Array} arr of numbers
- * @returns {number} sum of arr numbers
- */
-export function multiply(arr: number[]): number{
-    let mul = 1;
-    for(let ele of arr){
-        mul*=ele
+
+export function reverseArray(arr: string[]): string[] {
+    // Create a copy of the input array using slice() method
+    let copyArray = arr.slice();
+    // Reverse the copy of the array
+    let reversedString = copyArray.reverse();
+    return reversedString;
+}
+
+export function reverseArrayInPlace(arr:number[]):number[]{
+    let copyArray = arr.slice();
+    let reversedNumbers = copyArray.reverse();
+    return reversedNumbers;
+}
+
+export function scoreExams(studentAnswers: number[][], correctAnswers: number[]): number[] {
+    const scores: number[] = [];
+  
+    for (const student of studentAnswers) {
+      let score = 0;
+      for (let i = 0; i < student.length; i++) {
+        if (student[i] === correctAnswers[i]) {
+          score++;
+        }
+      }
+      scores.push(score);
     }
-    return mul;
+  
+    return scores;
+  }
 
-}
-/* findLongestWord */
-/**
- * takes an array of words and returns the length of the longest one
- * @param {*} arr of words 
- * @returns {number} length of longest word
- */
-export function findLongestWord(arr: string[]): number{
-    // for(let ele of arr){
-    //     for(let len of ele)[
-            
-    //     ]
-    // }
-    return 0;
-
-
-}
-
-/* 6. Write a function that takes two integers as inputs and returns a 2-dimensional array containing sequential numbers across each row as follows:
-describe("generate array", function () {
-    const expected33 = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]];
-    const expected23 = [ [1, 2, 3], [4, 5, 6]];
-    const expected21 = [ [1], [2]];
-     assert.deepEqual(generateArray(3,3), expected33); */
-
-/**
- * 
- * @param {*} rows num rows
- * @param {*} cols num cols
- * @returns {Array} 2d array with entries i + j
- */
-export function generateArray(rows: number, cols: number): number[][]{
-//IMPLEMENT THIS 
-
-return [[0]];
-}
+export function generateArray(rows: number, cols: number): number[][] {
+    const result: number[][] = [];
+  
+    for (let i = 0; i < rows; i++) {
+      result.push([]);
+      for (let j = 0; j < cols; j++) {
+        result[i].push(0);
+      }
+    }
+  
+    return result;
+  }
